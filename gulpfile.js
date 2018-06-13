9d0e279ec1910a16d13916cb26defedb9113cb02
Rob <robjmarks@gmail.com>
2018-06-13 15:54:39 +0100

    new css and gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,42 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch')
+var gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested'),
+  cssImport = require('postcss-import'),
+  browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  console.log("Hooray!")
-})
-
-gulp.task('html', function() {
-  console.log('html')
-})
+  gulp.task('default', function() {
+    console.log("Hooray!")
+  });
 
 gulp.task('styles', function() {
-  console.log("styles!")
-})
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('cssInject',['styles'], function(){
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
+});
 
 gulp.task('watch', function() {
 
-  watch('./app/index.html', function() {
-    gulp.start('html')
-  })
-  watch('./app/assets/styles/styles.css', function() {
-    gulp.start('styles')
-  })
-})
+  browserSync.init({
+    //notify:false,
+    server: {
+      baseDir:'app'
+    }
+  });
+
+  watch('./app/index.html',function(){
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject')
+  });
+
+});
