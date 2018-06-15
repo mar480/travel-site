a29a62a2282d094435fc41e2a784c71597fec562
Rob <robjmarks@gmail.com>
2018-06-15 19:22:21 +0100

    completed our features

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,19 +1,13 @@
 var gulp = require('gulp'),
-  watch = require('gulp-watch');
-  browserSync = require('browser-sync').create();
-
-
-gulp.task('cssInject', ['styles'], function() {
-  return gulp.src('./app/temp/styles/styles.css')
-    .pipe(browserSync.stream());
-});
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
 
   browserSync.init({
-    //notify:false,
+    notify: false,
     server: {
-      baseDir: 'app'
+      baseDir: "app"
     }
   });
 
@@ -22,7 +16,12 @@ gulp.task('watch', function() {
   });
 
   watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('cssInject')
+    gulp.start('cssInject');
   });
 
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
